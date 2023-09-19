5a700d6b8ed9e616f93a73313464106a2246f6ad
Matt Mastracci <matthew@mastracci.com>
2023-09-19 17:36:17 -0600

    feat: split_io (!Send) (#5)

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 mod flag;
 mod joinset;
+mod split;
 mod task;
 mod task_queue;
 
 pub use flag::Flag;
 pub use joinset::JoinSet;
+pub use split::split_io;
+pub use split::IOReadHalf;
+pub use split::IOWriteHalf;
 pub use task::spawn;
 pub use task::spawn_blocking;
 pub use task::JoinHandle;
@@ -12,3 +16,9 @@ pub use task::MaskFutureAsSend;
 pub use task_queue::TaskQueue;
 pub use task_queue::TaskQueuePermit;
 pub use task_queue::TaskQueuePermitAcquireFuture;
+
+/// Marker for items that are ![`Send`].
+#[derive(Copy, Clone, Default, Eq, PartialEq, PartialOrd, Ord, Debug, Hash)]
+pub struct UnsendMarker(
+  std::marker::PhantomData<std::sync::MutexGuard<'static, ()>>,
+);
