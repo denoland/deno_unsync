90bd0c2c15a9b0b1dc5ba1f70da9dde4f53ef011
David Sherret <dsherret@users.noreply.github.com>
2025-05-15 15:28:27 -0400

    feat(sync): add TaskQueue from CLI (#23)

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -1,9 +1,12 @@
 // Copyright 2018-2024 the Deno authors. MIT license.
 
 mod flag;
+mod task_queue;
 #[cfg(feature = "tokio")]
 mod value_creator;
 
 pub use flag::AtomicFlag;
+pub use task_queue::TaskQueue;
+pub use task_queue::TaskQueuePermit;
 #[cfg(feature = "tokio")]
 pub use value_creator::MultiRuntimeAsyncValueCreator;
